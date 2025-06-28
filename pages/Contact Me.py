bc117cfec59ef44e8f444a0b41d90f3b8d791c26
L0K10D1NS0N <karimgohary.gohary@gmail.com>
2025-06-28 17:16:33 +0300

    made the contact us page work

--- pages/Contact Me.py
+++ pages/Contact Me.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import send_emails
 
 st.header("Contact Me!")
+
+with st.form(key="Email-form"):
+    user_email = st.text_input("Your Email")
+    user_message = st.text_area("Message")
+    mail_message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{user_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_emails.send_email(mail_message)
+        st.info("Your mail was sent successfully!")
---
+++ send_emails.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context()
+
+    username = "karimgohary.gohary@gmail.com"
+    password = os.getenv("PORTFOLIO_EMAIL_PASSWORD")
+
+    receiver = "karimgohary.gohary@gmail.com"
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
